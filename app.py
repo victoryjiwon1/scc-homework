b8ec813b5a777e410a423ecbe0c6a6accb5cc052
victoryjiwon1 <75198633+victoryjiwon1@users.noreply.github.com>
2020-12-26 13:13:17 +0900

    모두의 책리뷰 연습

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+
+app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만듭니다.
+
+
+## HTML을 주는 부분
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+## API 역할을 하는 부분
+@app.route('/review', methods=['POST'])
+def write_review():
+    title_receive = request.args.get('title_give')
+    author_receive = request.args.get('author_give')
+    review_receive = request.args.get('review_give')
+
+    doc = {
+        'title': title_receive,
+        'author': author_receive,
+        'review': review_receive
+    }
+    db.bookreview.insert_one(doc)
+
+    return jsonify({'result': 'success', 'msg': '잘 저장 되었습니다.'})
+
+
+@app.route('/review', methods=['GET'])
+def read_reviews():
+    return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
